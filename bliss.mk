544f85f0a93941667040663f672f781bcec3894e
mr-tweaker <aniket.lamba@gmail.com>
2017-03-27 17:39:54 +0530

    armani: Blissify

--- bliss.mk
+++ bliss.mk
@@ -21,17 +21,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/armani/device.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/armani/armani-vendor.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Bliss stuff
+$(call inherit-product, vendor/bliss/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := armani
-PRODUCT_NAME := lineage_armani
+PRODUCT_NAME := bliss_armani
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := HM 1S
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=armani
+PRODUCT_BUILD_PROP_OVERRIDES +=
+    PRODUCT_NAME=armani \
+    ro.product.model=armani
